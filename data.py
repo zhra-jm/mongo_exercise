257a0dc63f28a0de1f74ae95a21fb3014a0dec77
zhra-jm <zahrajam1999@gmail.com>
2022-01-09 15:49:41 +0330

    whole-exercise

---
+++ data.py
@@ -0,0 +1,46 @@
+students = [
+    {'name': 'zahra',
+     'last_name': 'Jamshidi',
+     'phone_num': '09123456789'
+     },
+    {'name': 'ali',
+     'last_name': 'tahmasebi',
+     'phone_num': '09234567823'
+     },
+    {'name': 'alireza',
+     'last_name': 'malayeri',
+     'phone_num': '09126784678'
+     },
+    {'name': 'kamran',
+     'last_name': 'amiri',
+     'phone_num': '09305465765'
+     },
+    {'name': 'soheyla',
+     'last_name': 'hamidi',
+     'phone_num': '09386543454'
+     },
+    {'name': 'ayda',
+     'last_name': 'bashiri',
+     'phone_num': '09356784565'
+     },
+
+]
+
+courses = [
+    {'course': 'python',
+     'teacher': 'felani',
+     'price': '5000000',
+     'capacity': 10},
+    {'course': 'java',
+     'teacher': 'folani',
+     'price': '40000000',
+     'capacity': 15},
+    {'course': 'c++',
+     'teacher': 'yeki',
+     'price': '6000000',
+     'capacity': 8},
+    {'course': 'unity',
+     'teacher': 'oon yeki',
+     'price': '7000000',
+     'capacity': 20},
+]
